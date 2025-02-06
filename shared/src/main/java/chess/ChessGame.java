c91792c0f58190f2fd3fc4d93e2b5bae4668d4cb
Easton Crowther <easton.crowther@gmail.com>
2025-02-05 20:32:50 -0700

    fixed bug with stalemate, now checks for check

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -248,7 +248,9 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-
+        if (isInCheck(teamColor)) {
+            return false;
+        }
         for(int row = 1; row <= 8; row ++) {
             for(int col = 1; col <= 8; col ++) {
                 ChessPiece piece = chessBoard.getPiece(new ChessPosition(row, col));
