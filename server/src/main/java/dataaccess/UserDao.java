badd7f8c42d4b237a15fd0aa83b3c3acb1aa2111
Easton Crowther <easton.crowther@gmail.com>
2025-02-18 12:53:57 -0700

    added methods to UserDao

--- server/src/main/java/dataaccess/UserDao.java
+++ server/src/main/java/dataaccess/UserDao.java
@@ -1,4 +1,29 @@
 package dataaccess;
+import model.UserData;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class UserDao {
+    private Collection<UserData> users = new ArrayList<>();
+
+    public UserDao () {}
+    // create a new user
+    public void createUser (UserData user) {
+        this.users.add(user);
+    }
+    // retrieve a user with the given username
+    public UserData getUser (String username) {
+        for (UserData user : users) {
+            if (username.equals(user.username())) {
+                return user;
+            }
+        }
+        // no user exists with that username
+        return null;
+    }
+    // clears all users
+    public void clear () {
+        this.users.clear();
+    }
 }
