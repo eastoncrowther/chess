10059584e5e17a8dd9db69792c0d2bc47a4cbe7e
Easton Crowther <easton.crowther@gmail.com>
2025-02-20 11:35:48 -0700

    list test passing

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -1,6 +1,17 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +19,29 @@ class GameServiceTest {
 
     @Test
     void list() {
+        var gameDB = new MemoryGameDAO();
+        var authDB = new MemoryAuthDAO();
+
+        // create some chess games
+        var game1 = new ChessGame();
+        var game2 = new ChessGame();
+
+        try {
+            gameDB.createGame(new GameData(5, "Easton", "Canon", "Match1", game1));
+            gameDB.createGame(new GameData(6, "Canon", "Easton", "Match2", game2));
+            authDB.createAuth(new AuthData("1234", "Easton"));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        var gameService = new GameService(gameDB, authDB);
+        Collection<GameData> actualGames = gameService.list("1234");
+
+        Collection<GameData> expectedGames = new HashSet<>(Arrays.asList(
+                new GameData(5, "Easton", "Canon", "Match1", game1),
+                new GameData(6, "Canon", "Easton", "Match2", game2)
+        ));
+        Assertions.assertEquals(actualGames, expectedGames);
     }
 
     @Test
