5bc65880a12bd1ee52d908109de8f81688c7565c
Easton Crowther <easton.crowther@gmail.com>
2025-02-26 16:09:30 -0700

    began handler for registering a user

--- server/src/main/java/server/RegisterRequestHandler.java
+++ server/src/main/java/server/RegisterRequestHandler.java
@@ -1,4 +1,29 @@
 package server;
 
-public class RegisterRequestHandler {
+import com.google.gson.Gson;
+import org.eclipse.jetty.server.Authentication;
+import service.RegisterRequest;
+import service.RegisterResult;
+import service.UserService;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class RegisterRequestHandler implements Route {
+    UserService userService;
+
+    public RegisterRequestHandler (UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        Gson serializer = new Gson();
+
+        RegisterRequest registerRequest = serializer.fromJson(request.body(), RegisterRequest.class);
+
+        RegisterResult registerResult = userService.register(registerRequest);
+
+        return serializer.toJson(registerResult);
+    }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -23,8 +23,7 @@ public class Server {
         // Register your endpoints and handle exceptions here.
         Spark.post("/session", new LoginRequestHandler(userService));
 
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        Spark.post("/user", new RegisterRequestHandler(userService));
 
         Spark.awaitInitialization();
         return Spark.port();
