47f3544d7d41d63b655d71915a1c062c9cb2187e
Easton Crowther <easton.crowther@gmail.com>
2025-03-10 11:36:50 -0600

    downloaded dependencies/began implementing sqlAuthDao

---
+++ server/src/main/java/dataaccess/SqlAuthDao.java
@@ -0,0 +1,30 @@
+package dataaccess;
+
+import model.AuthData;
+
+public class SqlAuthDao implements AuthDAO {
+
+
+
+
+    
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(AuthData auth) throws DataAccessException {
+
+    }
+}
