aeadfaaab141427bd8c1d1e689198662b9b051c3
Easton Crowther <easton.crowther@gmail.com>
2025-03-30 13:22:11 -0600

    added files to begin implementing the websocket

---
+++ client/src/main/java/server/websocket/WebSocketFacade.java
@@ -0,0 +1,14 @@
+package server.websocket;
+import javax.websocket.*;
+
+
+
+
+public class WebSocketFacade extends Endpoint {
+
+    
+
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {}
+}
